626bee03b19621faeb6cf3373d438aa8743d22fb
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-06-27 16:55:27 +0000

    Set up a function that print square

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,14 +3,8 @@
 
 
 def say_my_name(first_name, last_name=""):
-    """Print a name.
-
-    Args:
-        first_name (str): The first name to print.
-        last_name (str): The last name to print.
-    Raises:
-        TypeError: If either of first_name or last_name are not strings.
-    """
+    """Print a name. """
+    
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
