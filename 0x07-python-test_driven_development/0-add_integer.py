c6e48128b7a4d37f34e9d3d223de4a6b4ea0a814
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-06-27 16:50:02 +0000

    Set up a function to print a user name

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,13 +3,8 @@
 
 
 def add_integer(a, b=98):
-    """Return the integer addition of a and b.
-
-    Float arguments are typecasted to ints before addition is performed.
-
-    Raises:
-        TypeError: If either of a or b is a non-integer and non-float.
-    """
+    """Return the integer addition of a and b."""
+    
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
     if ((not isinstance(b, int) and not isinstance(b, float))):
