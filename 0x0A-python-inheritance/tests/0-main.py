85a5ed711acd925f5bd9db248078495d32900643
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-07-04 12:55:31 +0000

    Tried to fixe pycodestyle test bugs

--- 0x0A-python-inheritance/tests/0-main.py
+++ 0x0A-python-inheritance/tests/0-main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 lookup = __import__('0-lookup').lookup
 
+
 class MyClass1(object):
     pass
 
@@ -8,6 +9,7 @@ class MyClass2(object):
     my_attr1 = 3
     def my_meth(self):
         pass
+
 
 print(lookup(MyClass1))
 print(lookup(MyClass2))
