5ea1bea75ad236c8bb5bc6e9fd54918b5080a2ae
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-07-04 21:55:36 +0000

    Tried to fixe some bugs

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-
-Rectangle = __import__("9-rectangle").Rectangle
+"""Defines a Rectangle subclass Square."""
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """Represent a square."""
 
     def __init__(self, size):
-        self.__intger_validator("size", size)
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
