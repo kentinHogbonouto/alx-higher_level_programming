ea9a4b31c06933a7fbba33e34aecb1f2469e56dd
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-07-04 12:50:46 +0000

    Tried to fixe pycodestyle test bugs

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
+
 class Rectangle:
     def __init__(self, width, height):
         self.__integer_validator("width", width)
