07cd1e1688ee0778a8f41ca51c5f95f9567877fb
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-07-05 20:09:36 +0000

    Tried to fixe some bugs

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """
-    Function  that appends a string at the end of 
-    a text file (UTF8) and returns the number of characters added
+Function  that appends a string at the end of 
+a text file (UTF8) and returns the number of characters added
 """
+
 
 def append_write(filename="", text=""):
     """script to append string in a text at the end"""
