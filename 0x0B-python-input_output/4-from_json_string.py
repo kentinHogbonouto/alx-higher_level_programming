39c9178e6fa5514829e267afdd9f978080ce0b82
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-07-05 20:31:54 +0000

    A function that returns an object (Python data structure) represented by a JSON string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A function that returns an object (Python data structure) represented by a JSON string"""
+import json
+
+def from_json_string(my_str):
+    """Script to transforme a string into json object"""
+
+    return json.load(my_str)
