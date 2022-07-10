bf6dfb01340f85d837a2cd3faf461118e1a2c9a6
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-07-10 16:37:35 +0000

    Tried to fixe some bugs

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -5,6 +5,9 @@ unittest
 
 import os
 import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 class TestBase_instantiation(unittest.TestCase):
     """Unittest for testing instantiation"""
