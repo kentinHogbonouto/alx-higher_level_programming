9d9d1290e6be5e7630fb3d8dca6ac847255a5b23
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-07-10 15:48:51 +0000

    Set up unit test folder

---
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+unittest
+"""
+
+import os
+import unittest
+
+class TestBase_instantiation(unittest.TestCase):
+    """Unittest for testing instantiation"""
+
+    def test_no_arg(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, b2.id - 1)
+
+    def test_three_bases(self):
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        self.assertEqual(b1.id, b2.id - 1, b3.id - 2)
+
+    def test_None_id(self):
+        b1 = Base(None)
+        b2 = Base(None)
+        self.assertEqual(b1.id, b2.id -1)
+
+    def test_unique_id(self):
+        self.assertEqual(12, Base(12).id)
+
+    def test_nb_instances_after_unique_id(self):
+        b1 = Base()
+        b2 = Base(12)
+        b3 = Base()
+        self.assertEqual(15, b1.id)
