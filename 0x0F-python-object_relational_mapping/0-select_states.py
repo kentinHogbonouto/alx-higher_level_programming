6ca328e5e37a66f11423adc8c2a7ce7873655eab
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-10-08 18:40:16 +0100

    fix : remove all unnecessary script

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,15 +6,9 @@ import sys
 import MySQLdb
 
 
-def connectToDB():
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("select * from states ORDER BY id ASC")
-    rows = cur.fetchAll()
-    for item in rows:
-        print(item)
-
-
-if __name__ == "__main__":
-    connectToDB()
+    cur.execute("select * FROM `states`")
+    [print(state) for state in cur.fetchAll()]
