31feb9fc6e1ae0c4bc464015c5ca469e71a31462
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-10-08 10:13:03 -0700

    fix : add necessary blanck line and execute file

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,6 +5,7 @@ Script that list all states from the database hbtn_0e_0_usa:
 import sys
 import MySQLdb
 
+
 def connectToDB():
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
@@ -13,6 +14,7 @@ def connectToDB():
     rows = cur.fetchAll()
     for item in rows:
         print(item)
+
 
 if __name__ == "__main__":
     connectToDB()
